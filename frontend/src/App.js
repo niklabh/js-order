22e3696e941d2e82473bdbf4422498ce95fbd83b
Nikhil Ranjan <niklabh811@gmail.com>
2020-11-15 23:18:55 +0530

    css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 
+import { Style } from './style';
 import Login from './screens/login';
 import Orders from './screens/orders';
 import Order from './screens/order';
@@ -9,22 +10,25 @@ import './firebase';
 
 function App() {
   return (
-    <Router>
-      {/* <MenuBar /> */}
-      <Container>
-        <Switch>
-          <Route exact path="/">
-            <Login/>
-          </Route>
-          <Route path="/orders">
-            <Orders/>
-          </Route>
-          <Route path="/order/:id">
-            <Order/>
-          </Route>
-        </Switch>
-      </Container>
-    </Router>
+    <>
+      <Style />
+      <Router>
+        {/* <MenuBar /> */}
+        <Container>
+          <Switch>
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <Route path="/orders">
+              <Orders />
+            </Route>
+            <Route path="/order/:id">
+              <Order />
+            </Route>
+          </Switch>
+        </Container>
+      </Router>
+    </>
   );
 }
 
