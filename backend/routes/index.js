054b2c4d0689408ef2e836fc1302195143ced47f
Nikhil Ranjan <niklabh811@gmail.com>
2020-11-16 19:16:17 +0530

    backend

---
+++ backend/routes/index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = express.Router();
+const admin = require('firebase-admin');
+const { uid } = require('uid');
+
+router.post('/orders', async (req, res, next) => {
+  try {
+    const db = admin.database();
+    const ref = db.ref('orders');
+    const orderRef = ref.child(uid(25));
+
+    const payload = {
+      address: {
+        city: req.body.address.city,
+        country: req.body.address.country,
+        street: req.body.address.street,
+        zip: req.body.address.zip
+      },
+      bookingDate: req.body.bookingDate,
+      customer: {
+        email: req.body.customer.email,
+        name: req.body.customer.name,
+        phone: req.body.customer.phone
+      },
+      title: req.body.title,
+      uid: req.body.uid
+    };
+
+    await orderRef.set(payload);
+
+    res.json(payload);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    });
+  }
+});
+
+router.put('/orders/:id', async (req, res, next) => {
+  try {
+    const db = admin.database();
+    const ref = db.ref('orders');
+    const orderRef = ref.child(req.params.id);
+
+    const payload = {
+      address: {
+        city: req.body.address.city,
+        country: req.body.address.country,
+        street: req.body.address.street,
+        zip: req.body.address.zip
+      },
+      bookingDate: req.body.bookingDate,
+      customer: {
+        email: req.body.customer.email,
+        name: req.body.customer.name,
+        phone: req.body.customer.phone
+      },
+      title: req.body.title,
+      uid: req.body.uid
+    };
+
+    await orderRef.update(payload);
+
+    res.json(payload);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    });
+  }
+});
+
+module.exports = router;
